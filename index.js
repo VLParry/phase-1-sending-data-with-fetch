15c25a3bf18781997057d6dd6313cde55a171d31
Victoria Parry <vlparry@gmail.com>
2022-05-21 11:33:40 -0400

    commit tree 2658f0137e18c367c3a22610cfea1674d6d75b60

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your code here
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }),
+// }
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
+
+
+
+
+
+
+
+function submitData(name, email) {
+    const body = document.querySelector('body')
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({ name, email })
+    })
+
+        .then(res => res.json())
+        .then(data => body.append(data.id))
+        .catch(function (error) {
+            alert("Unauthorized Access")
+            body.append(error.message)
+        })
+}
